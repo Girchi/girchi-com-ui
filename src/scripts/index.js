76fa43698ce3364f14cd2fee117368852f12108e
David Babunashvili <>
2019-04-16 00:19:19 +0400

    add GeD progress

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,12 +3,36 @@ import '../index.html'
 import '../styles/index.scss'
 import 'bootstrap/js/src/index'
 
-// Checkbox
+$(document).ready(() => {
+  // Checkbox
+  $('.form-checkbox-input [type="checkbox"]').on('change', (e) => {
+    const parent = $(e.target).parent()
+    if (e.target.checked) {
+      parent.addClass('checked')
+    } else {
+      parent.removeClass('checked')
+    }
+  })
 
-$('.form-checkbox-input [type="checkbox"]').on('change', (e) => {
-  if (e.target.checked) {
-    $(e.target).parent().addClass('checked')
-  } else {
-    $(e.target).parent().removeClass('checked')
+  // GeD Progress
+  const renderGeDProgress = () => {
+    const windowWidth = $(window).width()
+    const progressBarGutter = 50
+    const gedProgress = $('.js-ged-progress')
+    const gedProgressBar = gedProgress.find('.progress-bar')
+    const gedProgressTotal = gedProgress.find('.progress-bar-total')
+    const gedProgressTotalWidth = gedProgressTotal.width() + progressBarGutter
+    const gedProgressValue = gedProgressBar.attr('aria-valuenow')
+    const gedProgressBarWidth = windowWidth * gedProgressValue / 100
+
+    gedProgressBar.css({ width: `${gedProgressValue}%` })
+    // if gedProgressValue is closer to gedProgressTotal
+    if (windowWidth - gedProgressTotal.width() - progressBarGutter < gedProgressBarWidth) {
+      // add padding right to progress bar label
+      const paddingRight = gedProgressTotalWidth * (gedProgressValue / 100)
+      gedProgressBar.css({ paddingRight })
+    }
   }
+  setTimeout(renderGeDProgress, 500)
+  $(window).on('resize', renderGeDProgress)
 })
