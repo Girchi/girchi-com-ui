317f30713f68fc4ec98dfda207f09b21686c306f
David Babunashvili <>
2019-04-20 16:34:52 +0400

    add large news carousel

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
     responsive: {
       0: {
         items: 1,
-        margin: 0,
         slideBy: 1
       },
       480: {
@@ -33,5 +32,55 @@ $(document).ready(function () {
   })
   $('.video-carousel-prev').on('click', () => {
     videoCarousel.trigger('prev.owl.carousel')
+  })
+
+  // News carousel
+  const newsCarousel = $('.news-carousel').owlCarousel({
+    margin: 0,
+    items: 1,
+    dots: true,
+    touchDrag: true,
+    mouseDrag: false,
+    autoplay: true,
+    animateOut: 'fadeOut',
+    dotsContainer: $('.news-carousel-dots')
+  })
+  $('.news-carousel-next').on('click', () => {
+    newsCarousel.trigger('next.owl.carousel')
+  })
+  $('.news-carousel-prev').on('click', () => {
+    newsCarousel.trigger('prev.owl.carousel')
+  })
+
+  // Rating of politicians carousel
+
+  const ratingOfPoliticiansCarousel = $('.rating-of-politicians-carousel').owlCarousel({
+    margin: 15,
+    dots: true,
+    dotsContainer: $('.rating-of-politicians-carousel-dots'),
+    responsive: {
+      0: {
+        items: 1,
+        slideBy: 1
+      },
+      480: {
+        items: 2,
+        slideBy: 2
+      },
+      768: {
+        items: 3,
+        slideBy: 3
+      },
+      992: {
+        items: 5,
+        slideBy: 5
+      }
+    }
+  })
+  $('.rating-of-politicians-carousel-next').on('click', () => {
+    ratingOfPoliticiansCarousel.trigger('next.owl.carousel')
+  })
+  $('.rating-of-politicians-carousel-prev').on('click', () => {
+    ratingOfPoliticiansCarousel.trigger('prev.owl.carousel')
   })
 })
