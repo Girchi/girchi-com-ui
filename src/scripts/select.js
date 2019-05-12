3c9f18f3861060d0199d457b9dc832be06ac5e4a
David Babunashvili <unknown>
2019-05-12 23:21:18 +0400

    add donation page

--- src/scripts/select.js
+++ src/scripts/select.js
@@ -4,3 +4,11 @@ import 'bootstrap-select/sass/bootstrap-select.scss'
 $('.select').selectpicker({
   showContent: true
 })
+
+$('.form-control select').on('change', (e) => {
+  if (e.target.value.toString().length) {
+    $(e.target).addClass('selected')
+  } else {
+    $(e.target).removeClass('selected')
+  }
+})
