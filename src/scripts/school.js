3b45792c93a0d5e7462b43f0212acab78ad3982c
David Babunashvili <>
2019-06-26 22:03:01 +0400

    change monthly price calculation

--- src/scripts/school.js
+++ src/scripts/school.js
@@ -1,10 +1,11 @@
 
 import 'bootstrap-slider'
 import 'bootstrap-slider/src/sass/bootstrap-slider.scss'
+import { round } from './helpers'
 
 $('#pupilRegistration').on('shown.bs.modal', function (e) {
   const unit = $('.time-range-picker').attr('data-slider-unit')
-  const price = parseFloat($('.time-range-picker').attr('data-slider-price'))
+  const price = parseFloat($('.time-range-picker').attr('data-slider-price')) / 12
   const labels = JSON.parse($('.time-range-picker').attr('data-slider-ticks-labels'))
   const ticks = JSON.parse($('.time-range-picker').attr('data-slider-ticks'))
   const monthlyPrice = $('.monthly-price')
@@ -38,7 +39,7 @@ $('#pupilRegistration').on('shown.bs.modal', function (e) {
         $('.time-range-picker').slider('setValue', e.value.oldValue)
       }
       $('.tick-slider-selection').text(`${diff} ${unit}`)
-      monthlyPrice.text(diff * price)
+      monthlyPrice.text(round(diff * price, 2))
       startTime.val(start)
       endTime.val(end)
     })
@@ -47,13 +48,13 @@ $('#pupilRegistration').on('shown.bs.modal', function (e) {
     const defaultValue = $('.time-range-picker').slider('getValue')
     const diff = Math.abs(defaultValue[0] - defaultValue[1])
     $('.tick-slider-selection').text(`${diff} ${unit}`)
-    monthlyPrice.text(diff * price)
+    monthlyPrice.text(round(diff * price, 2))
     // update form control
     startTime.val(defaultValue[0])
     endTime.val(defaultValue[1])
   } else {
     const diff = Math.abs(startTime.val() - endTime.val())
-    monthlyPrice.text(diff * price)
+    monthlyPrice.text(round(diff * price, 2))
 
     startTime.on('change', () => {
       let start = parseFloat(startTime.val())
@@ -68,7 +69,7 @@ $('#pupilRegistration').on('shown.bs.modal', function (e) {
         end = parseFloat(endTime.val())
         diff = Math.abs(start - end)
       }
-      monthlyPrice.text(diff * price)
+      monthlyPrice.text(round(diff * price, 2))
     })
     endTime.on('change', () => {
       let start = parseFloat(startTime.val())
@@ -83,7 +84,7 @@ $('#pupilRegistration').on('shown.bs.modal', function (e) {
         end = parseFloat(endTime.val())
         diff = Math.abs(start - end)
       }
-      monthlyPrice.text(diff * price)
+      monthlyPrice.text(round(diff * price, 2))
     })
   }
 })
