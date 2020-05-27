c5d02a49a1b2f534eb8f6110f5165d8303471e6a
David Babunashvili <>
2020-05-27 19:22:56 +0400

    add country field to profile page and add select all to users table

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -9,4 +9,57 @@ $(function () {
       : 0
     outputEl.text(value * rate)
   })
+
+  $('.js-check-all').on('change', function () {
+    $(this)
+      .parents('table')
+      .find('tbody input[type="checkbox"]')
+      .prop('checked', $(this).is(':checked'))
+
+    if ($(this).is(':checked')) {
+      $(this)
+        .parents('table')
+        .find('tbody .form-checkbox-input')
+        .addClass('checked')
+    } else {
+      $(this)
+        .parents('table')
+        .find('tbody .form-checkbox-input')
+        .removeClass('checked')
+    }
+  })
+
+  $('.js-check-all')
+    .parents('table')
+    .find('tbody .form-checkbox-input')
+    .on('change', function () {
+      const checked = $(this)
+        .parents('table')
+        .find('tbody input[type="checkbox"]:checked')
+      const all = $(this)
+        .parents('table')
+        .find('tbody input[type="checkbox"]')
+
+      if (checked.length === all.length) {
+        $(this)
+          .parents('table')
+          .find('.js-check-all')
+          .prop('checked', true)
+
+        $(this)
+          .parents('table')
+          .find('thead .form-checkbox-input')
+          .addClass('checked')
+      } else {
+        $(this)
+          .parents('table')
+          .find('.js-check-all')
+          .prop('checked', false)
+
+        $(this)
+          .parents('table')
+          .find('thead .form-checkbox-input')
+          .removeClass('checked')
+      }
+    })
 })
