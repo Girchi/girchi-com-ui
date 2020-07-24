bd3c5fc6b161cdf013e58b3295808eaee32c61c4
gvantsa osepashvili <osefashviligvanca@gmail.com>
2020-07-24 21:25:15 +0400

    defence-changes

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,17 +1,12 @@
-// Cache selectors
 var lastId,
     topMenu = $("#scroll-btn"),
     topMenuHeight = topMenu.outerHeight() + 1,
-    // All list items
     menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
     scrollItems = menuItems.map(function () {
         var item = $($(this).attr("href"));
         if (item.length) { return item; }
     });
 
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
 menuItems.click(function (e) {
     var href = $(this).attr("href"),
         offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
